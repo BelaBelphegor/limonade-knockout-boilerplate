d2459b541b1d9590d14937e61d8a82b89c062c66
BelaBelphegor <tiboitel@student.42.fr>
2017-04-30 01:58:38 +0200

    Updating AppViewModel

--- public/index.html.php
+++ public/index.html.php
@@ -2,3 +2,5 @@
 ?>
 <p>First name: <input data-bind="value: firstName"/></p>
 <p>Last name: <input data-bind="value: lastName"/></p>
+<p>Full name: <input data-bind="value: fullName"/></p>
+<button data-bind="click: capitalizeLastName">GO CAPS!</button>
--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,14 @@ var AppViewModel = function(first, last)
     this.firstName = ko.observable(first);
     this.lastName = ko.observable(last);
     this.fullName = ko.pureComputed(function() {
-        return this.firstName() + " " + this.lastName;
+        return this.firstName() + " " + this.lastName();
     }, this);
+    
+    this.capitalizeLastName = function()
+    {
+        var currentValue = this.lastName();
+        this.lastName(currentValue.toUpperCase());
+    };
 };
 
 ko.applyBindings(new AppViewModel("Planet", "Earth"));
