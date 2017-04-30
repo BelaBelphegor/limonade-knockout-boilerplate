4289530e2141b04e6b17439da536115579cd0616
Timothee <tiboitel@student.42.fr>
2017-04-30 21:40:30 +0200

    Updating app.js with example of REST request

--- public/index.html.php
+++ public/index.html.php
@@ -1,6 +1,11 @@
 <?php
 ?>
-<p>First name: <input data-bind="value: firstName"/></p>
-<p>Last name: <input data-bind="value: lastName"/></p>
-<p>Full name: <input data-bind="value: fullName"/></p>
-<button data-bind="click: capitalizeLastName">GO CAPS!</button>
+<section data-bind="with: chosenData">
+	<table data-bind="width: chosenData">
+		<thead><tr><th>Name<th><th>Stats</th></tr></thead>
+	</tbody>
+		<tr><td data-bind="text: name"></td>
+		<td data-bind="foreach: stats"><span data-bind="text: base_stat"> </span></td></tr>
+
+	</table>
+</section>
--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,36 @@
-var AppViewModel = function(first, last)
+var AppViewModel = function()
 {
-    this.firstName = ko.observable(first);
-    this.lastName = ko.observable(last);
-    this.fullName = ko.pureComputed(function() {
-        return this.firstName() + " " + this.lastName();
-    }, this);
-    
-    this.capitalizeLastName = function()
-    {
-        var currentValue = this.lastName();
-        this.lastName(currentValue.toUpperCase());
-    };
+	var self = this;
+	self.chosenDataID = ko.observable();
+	self.chosenData = ko.observable();
+	self.chosenSubdata = ko.observable();
+
+	self.goToData = function(data)
+	{
+		location.hash = data;
+	}
+
+	self.goToSubdata = function(subdata)
+	{
+		location.hash = subdata.data + '/' + subdata.id;
+	}
+	self.goToData("1");
+
+	Sammy(function()
+	{
+		this.get('#:data', function()
+		{
+			self.chosenDataID(this.params.data);
+			self.chosenData(null);
+			$.get("http://pokeapi.co/api/v2/pokemon/" + this.params.data, {data: this.params.data }, self.chosenData), "json";
+		});
+
+		this.get('#:data/:subdataid', function()
+		{
+			return;
+		});
+	}).run();
 };
 
-ko.applyBindings(new AppViewModel("Planet", "Earth"));
+ko.applyBindings(new AppViewModel());
+
